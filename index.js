176b14c34d8f1df400ed9d98f2ba1e34206a925a
kael <kaelnehrom@gmail.com>
2020-01-24 22:39:01 +0200

    remove node.js dep

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
 /*
-* @Author: zyc
-* @Date:   2016-09-11 23:36:05
-* @Last Modified by:   unrealce
-* @Last Modified time: 2016-09-13 01:26:13
-*/
-'use strict'
+ * @Author: zyc
+ * @Date:   2016-09-11 23:36:05
+ * @Last Modified by:   unrealce
+ * @Last Modified time: 2016-09-13 01:26:13
+ */
+"use strict";
 
-const crypto = require('crypto')
-const base58 = require('bs58')
+const base58 = require("bs58");
 
+var sha256 = require("crypto-js/sha256");
+var CryptoJs = require("crypto-js");
 
-module.exports.encode = (data, prefix = '00', encoding = 'hex') => {
-  if (typeof data === 'string') {
-    data = new Buffer(data, encoding)
+function toWordArray(buf) {
+  return CryptoJs.lib.WordArray.create(buf);
+}
+
+function toBuffer(wa) {
+  return Buffer.from(wa.toString(CryptoJs.enc.Hex), "hex");
+}
+
+function sha256x2(buffer) {
+  var wa = toWordArray(buffer);
+  var tmp = sha256(wa);
+  return toBuffer(sha256(tmp));
+}
+
+module.exports.encode = (data, prefix = "00", encoding = "hex") => {
+  if (typeof data === "string") {
+    data = new Buffer(data, encoding);
   }
   if (!(data instanceof Buffer)) {
-    throw new TypeError('"data" argument must be an Array of Buffers')
+    throw new TypeError('"data" argument must be an Array of Buffers');
   }
   if (!(prefix instanceof Buffer)) {
-    prefix = new Buffer(prefix, encoding)
+    prefix = new Buffer(prefix, encoding);
   }
-  let hash = Buffer.concat([prefix, data])
-  hash = crypto.createHash('sha256').update(hash).digest()
-  hash = crypto.createHash('sha256').update(hash).digest()
-  hash = Buffer.concat([prefix, data,  hash.slice(0, 4)])
-  return base58.encode(hash)
-}
+  let hash = Buffer.concat([prefix, data]);
+  hash = sha256x2(hash);
+  hash = Buffer.concat([prefix, data, hash.slice(0, 4)]);
+  return base58.encode(hash);
+};
 
 module.exports.decode = (string, encoding) => {
-  const buffer = new Buffer(base58.decode(string))
-  let prefix = buffer.slice(0, 1)
-  let data = buffer.slice(1, -4)
-  let hash = Buffer.concat([prefix, data])
-  hash = crypto.createHash('sha256').update(hash).digest()
-  hash = crypto.createHash('sha256').update(hash).digest()
+  const buffer = new Buffer(base58.decode(string));
+  let prefix = buffer.slice(0, 1);
+  let data = buffer.slice(1, -4);
+  let hash = Buffer.concat([prefix, data]);
+  hash = sha256x2(hash);
   buffer.slice(-4).forEach((check, index) => {
     if (check !== hash[index]) {
-      throw new Error('Invalid checksum')
+      throw new Error("Invalid checksum");
     }
-  })
+  });
   if (encoding) {
-    prefix = prefix.toString(encoding)
-    data = data.toString(encoding)
+    prefix = prefix.toString(encoding);
+    data = data.toString(encoding);
   }
-  return { prefix, data }
-}
+  return { prefix, data };
+};
